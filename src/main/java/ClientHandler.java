5bf535f63a9fe22912f505c919b3134cb28a757a
DonnyLp <dondariolapointe8@gmail.com>
2024-05-30 12:45:44 -0400

    Refactoring the Client Handler for modularization and added functonality to handle POST requests.

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -12,8 +12,9 @@ public class ClientHandler implements Runnable {
     
     private final Socket clientSocket;
     private String directoryPath;
+
     // enum that defines different avaliable operations served to the client
-    private enum  serverOperation{           
+    private enum serverOperation{           
         ECHO,
         USER_AGENT,
         EMPTY,
@@ -66,6 +67,7 @@ public class ClientHandler implements Runnable {
                 //Further spliting used for parsing parts of each request section i.e path
                 String [] requestSplit = requestString.split("\r\n",0); 
                 String [] requestLine = requestSplit[0].split(" ", 0); 
+                String requestType = requestLine[0]; 
                 String requestURI = requestLine[1]; 
                 
                 String echoString; 
@@ -76,7 +78,7 @@ public class ClientHandler implements Runnable {
                 
 
                 //All operations supported for responses
-                switch (checkOperationType(requestURI)) {
+                switch (defineServerOperation(requestURI)) {
                     case ECHO ->{
                         echoString = requestURI.substring(6);
                         clientSocket.getOutputStream().write(("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
@@ -92,7 +94,7 @@ public class ClientHandler implements Runnable {
                         clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     }
                     case FILE ->{
-                        handleGetFileRequest(directoryPath, clientSocket, requestURI);
+                        handleGetFileRequest(directoryPath, clientSocket, requestString,requestType);
                     }
                     case INVALID_REQUEST->{
                         clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n".getBytes());
@@ -102,13 +104,56 @@ public class ClientHandler implements Runnable {
                 clientSocket.close();
     }
 
+
+
+    private void handleGetFileRequest(String directoryPath, Socket clientSocket, String requestURI, String requestType) throws FileNotFoundException, IOException{
+        
+        //Construct the newly wanted absolute path for the given filename
+        
+        String fileName = requestURI.split("/")[2];
+        directoryPath += fileName;
+
+        //Create a directory instance
+        Path directory = Path.of(directoryPath);
+        char [] fileBuffer = new char[1024];
+        FileReader fileReader;
+
+        boolean fileExists = Files.exists(directory);
+            
+            if(fileExists){
+
+                //Jump to the appropriate request type 
+                switch (requestType) {
+                    case "GET" -> {
+                    //Create a new fileReader instance for the existing file
+                    fileReader = new FileReader(directoryPath);
+                    fileReader.read(fileBuffer);
+                    String fileContents = new String(fileBuffer).trim();
+                
+                    //Respond with content length, cotent type: application/octet-stream, and body as the files contents
+                    clientSocket.getOutputStream().write(("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length:" +
+                    fileContents.length() + "\r\n\r\n" + fileContents + "\r\n").getBytes());
+                    fileReader.close();
+
+                    }
+                    case "POST" -> {
+                        //Create a new filewriter instance to write to the given filename
+                    }
+                    default -> throw new HttpServerException("Request Type" + requestType + " is currently not supported");
+                };
+
+            }else{
+                clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n".getBytes());
+            } 
+        }
+
      /*
-     helper that validates which operation is to be executed by the server
-    @param path The path present in the request line
-    @return serverOperation An enum constant is provided depending on the given path
+      * helper that validates which operation is to be executed by the server
+      * @param path The path present in the request line
+      * @return serverOperation An enum constant is provided depending on the given path
      */
 
-    private serverOperation checkOperationType(String requestURI){
+    private serverOperation defineServerOperation(String requestURI){
         serverOperation operation;
 
         if(requestURI.contains("/echo")) operation = serverOperation.ECHO;
@@ -119,36 +164,14 @@ public class ClientHandler implements Runnable {
         return operation;
     }
 
-    private void handleGetFileRequest(String directoryPath, Socket clientSocket, String requestURI) throws FileNotFoundException, IOException{
-        
-        //Construct the newly wanted absolute path for the given filename
-        String fileName = requestURI.split("/")[2];
-        directoryPath += fileName;
+    private String [] getRequestLine(String requestString){ 
+        String [] requestSplit = requestString.split("\r\n",0); 
+        return requestSplit[0].split(" ", 0);
+    }
 
-        //Create a directory instance to get the file
-        Path directory = Path.of(directoryPath);
-        char [] fileBuffer = new char[1024];
-        FileReader fileReader;
+    private String getRequestURI(String requestString){
+        String requestUri = getRequestURI(requestString);
+        return requestUri;
+    }
 
-
-        boolean fileExists = Files.exists(directory);
-            
-            if(fileExists){
-
-                //Create a new fileReader instance for the existing file
-                fileReader = new FileReader(directoryPath);
-                fileReader.read(fileBuffer);
-                String fileContents = new String(fileBuffer).trim();
-
-                //Respond with content length, cotent type: application/octet-stream, and body as the files contents
-                clientSocket.getOutputStream().write(("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length:" +
-                        fileContents.length() + "\r\n\r\n" + fileContents + "\r\n").getBytes());
-                
-                fileReader.close();
-            }else{
-                clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n".getBytes());
-            }
-            
-        }
-    
 }
